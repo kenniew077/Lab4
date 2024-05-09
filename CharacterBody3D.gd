b2c2258ae5cd7f56382390cbb048924aa0d61cca
kenniew077 <159483921+kenniew077@users.noreply.github.com>
2024-05-08 21:52:21 -0400

    update

---
+++ CharacterBody3D.gd
@@ -0,0 +1,16 @@
+extends CharacterBody3D
+
+var speed = 10.0
+
+func _process(delta):
+	var direction = Vector3.ZERO
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		direction.z -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.z += 1
+	direction = direction.normalized()
+	
